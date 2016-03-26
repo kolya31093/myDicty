2589282c7c949be83ded0b944097e0718cd3f07b
kolya31093 <kolya31093@ro.ru>
2016-03-26 10:41:52 +0200

    Please enter the commit message for your changes. Lines starting  vendor

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'AIg_StCRdNoxgB0kd5t6iuSaUDME1V33',
+            'cookieValidationKey' => '1',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
